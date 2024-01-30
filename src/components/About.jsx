b325aa86c650e4261f68dff4a49b6d16de9959fa
Abhishek kumar Gupta <114441358+guptaabhishekgupta11@users.noreply.github.com>
2024-01-30 23:34:15 +0530

    Here in this project i have Generated the REST API using Spring boot and I have fetched the REST API using React js.

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React,{useEffect} from 'react'
+
+const About = () => {
+  useEffect(() => {
+    document.title = 'Add New Course';
+  })
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
